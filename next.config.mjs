cbf52e08c756e8bedf4d2e86de081fbd26884b10
Andres felipe Theran forero <andrestheran@outlook.com>
2025-04-28 00:21:49 -0500

    fixing vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const config = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  reactStrictMode: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 export default config;
